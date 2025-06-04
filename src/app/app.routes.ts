5d76409da63f1abe790263447246c5954d265bef
denv77 <32192222+denv77@users.noreply.github.com>
2025-06-04 15:40:12 +0300

    Add GitHub Pages deployment

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { MicrofrontendTableComponent } from './components/microfrontend-table/microfrontend-table.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: MicrofrontendTableComponent,
+  },
+];
